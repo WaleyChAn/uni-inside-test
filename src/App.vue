ef63022ac4bc38963323a05c8747750c1c5acac5
陈伟立 <chenweili@ourway.net>
2023-10-16 12:14:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    const emitData = () => {
+      const uniSdk = uni;
+      const info = {
+        name: "wft",
+        age: 18,
+      };
+      console.log(uniSdk, info);
+      uniSdk.postMessage({
+        data: {
+          info,
+        },
+      });
+    };
+    return {
+      emitData,
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <button @click="emitData">子传父</button>
+  </div>
+</template>
+
+<style scoped></style>
